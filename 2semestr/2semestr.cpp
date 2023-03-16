fada37304cb88098c0e7e377d2cee65d9fcd3e41
daniya_kuspanova <kyspanova@icloud.com>
2023-03-16 18:34:21 +0400

    add class binary search tree  and add starting little main

--- 2semestr/2semestr.cpp
+++ 2semestr/2semestr.cpp
@@ -1,125 +1,173 @@
-﻿#include <iostream>
-//практика доделать дома
-//template<class T>
-//struct Point {
-//    T x = 0, y = 0;
-//};
-//template<class T>
-//struct List {
-//    Point point;
-//    List* next;
-//};
-//template<class T>
-//List<T>* task1(List<T>* head, Point<T>p, T e)
-//{
-//    List<T>* l = head;
-//    while (head) {
-//        x = head->point.x - p.x;
-//        y = head->point.y - p.y;
-//        if (sqrt(pow(x, 2) + pow(y, 2)) < e) {
-//            l->point.x = head->point.x;
-//            l->point.y = head->point.y;
-//            l = l->next;
-//        }
-//        head = head->next;
-//    }
-//    return l;
-//}
-//template<class T>
-//List<T>* task1(List<T>* head)
-//{
-//    T len = 0, m = 0;
-//    List<T>* l = head->next;
-//    List<T>* new_list = new List<T>;
-//    while (head) {
-//        while (l) {
-//            x = head->point.x - l->point.x;
-//            y = head->point.y - l->point.y;
-//            m = sqrt(pow(x, 2) + pow(y, 2));
-//            if ( m > len) {
-//                len = m;
-//                new_list->point.x = head->point.x;
-//                new_list->point.y = head->point.y;
-//                new_list->next->point.x = l->point.x;
-//                new_list->next->point.y = l->point.y;
-//            }
-//            l = l->next;
-//        }
-//        head = head->next;
-//        l = head->next;
-//    }
-//    return new_list;
-//}
+﻿//Класс должен предоставлять, как минимум, следующие функции :
 //
-//
-////дз: "вставка в упорядоченный список с сохранением порядка"
-//template<class T>
-//struct List {
-//    T data;
-//    List* next;
-//};
-//template<class T>
-//List<T>* task(List<T>* head, T data) {
-//    List<T>* pre = head;
-//    List<T>* new_head = head;
-//    head = head->next;
-//    List<T>* tmp = new List<T>;
-//    tmp->data = data;
-//    while (pre)
-//    {
-//        if (data < pre->data) {
-//            tmp->next = pre;
-//            return tmp;
-//        }
-//        if (data >= pre->data && data <= head->data) {
-//            pre->next = tmp;
-//            tmp->next = head;     
-//            return new_head;
-//        }
-//        if (head->next == NULL && data >= pre->data)
-//        {
-//            pre->next = tmp;
-//            tmp->next = NULL;
-//            return new_head;
-//        }
-//        pre = pre->next;
-//        head = head->next;
-//    }
-//    return new_head;
-//}
-//
-
-
-//дз: "проверьте связный список на симметричность", но уже не двусвязный
-template<class T>
-struct List {
-    T data;
-    List* next;
+//конструктор копирования;
+//деструктор;
+//оператор присваивания;
+//void print() – печать содержимого;
+//bool insert(int key) – вставка элемента;
+//bool contains(int key) - проверка наличия элемента;
+//bool erase(int key) – удаление элемента;
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <locale.h>
+#include <iostream>
+using namespace std;
+struct Leaf {
+    int data;
+    Leaf* left;
+    Leaf* right;
 };
-template<class T>
-bool task(List<T>* head) {
-    List<T>* list = head;
-    List<T>* l = head;
-    int n = 0;
-    while (l) {
-        n++;
-    }
-    T* mass = new T[n];
-    int i = 0;
-    while (list) {
-        mass[i] = list->data;
-        list = list->next;
-        i++;
-    }
-    i--;
-    for (int j = 0; ; j++, i--) {
-        if (mass[j] != mass[i]) return false;
-        if ((j + 1 == i && i - 1 == j) || j == i) return true;
-    }
-}
-
-int main()
+class Tree
 {
-    std::cout << "Hello World!\n";
+private:
+    Leaf* root;
+    bool insert_leaf(Leaf*& head, int val)
+    {
+        if (!head)
+        {
+            head = new Leaf;
+            head->data = val;
+            head->left = head->right = NULL;
+            return true;
+        }
+        else if (val < head->data)
+            insert_leaf(head->left, val);
+        else if (val > head->data)
+            insert_leaf(head->right, val);
+        else
+            return false;
+    }
+    bool contains_leaf(const Leaf* head, int val)
+    {
+        if (!head)
+            return false;
+        else if (val < head->data)
+            return contains_leaf(head->left, val);
+        else if (val > head->data)
+            return contains_leaf(head->right, val);
+        else
+            return true;
+    }
+    void print_leaf(const std::string& prefix, const Leaf* node, bool isLeft)
+    {
+        if (node)
+        {
+            std::cout << prefix;
+
+            //std::cout << (isLeft ? "├──" : "└──");
+            std::cout << (isLeft ? "|: " : "|: ");
+
+            // print the value of the node
+            std::cout << node->data << std::endl;
+
+            // enter the next tree level - left and right branch
+            print_leaf(prefix + (isLeft ? "|   " : "    "), node->left, true);
+            print_leaf(prefix + (isLeft ? "|   " : "    "), node->right, false);
+        }
+    }
+    Leaf* minimum(Leaf* head)
+    {
+        while (head->left) head = head->left;
+        return head;
+    }
+    bool erase_leaf(Leaf* head, int val) {
+        if (!head) return false;
+        if (val < head->data)
+            erase_leaf(head->left, val);
+        else if (val > head->data)
+            erase_leaf(head->right, val);
+        else {
+            if (!head->left) {
+                Leaf* leaf = head->right;
+                delete head;
+                return true;
+            }
+            else if (!head->right) {
+                Leaf* leaf = head->left;
+                delete head;
+                return true;
+            }
+            Leaf* leaf = minimum(head->right);
+            head->data = leaf->data;
+            erase_leaf(head->right, leaf->data);
+            return true;
+        }
+    }
+    void clear(Leaf*& head) //+
+    {
+        if (!head) return;
+        clear(head->left);
+        clear(head->right);
+        delete head;
+    }
+    void copy(Leaf* a, const Leaf* b) //+
+    {
+        if (!a)
+        {
+            b = NULL;
+            return;
+        }
+        a = new Leaf;
+        a->data = b->data;
+        copy(a->left, b->left);
+        copy(a->right, b->right);
+    }
+public:
+    Tree(); //+
+    Tree(const Tree&); //+
+    ~Tree();//+
+    bool insert(int);//+
+    bool contains(int x); //+
+    bool erase(int x); //+
+    void print(); //+
+    Tree& operator=(const Tree& src);
+};
+Tree::Tree() :root(NULL) {}
+Tree& Tree::operator=(const Tree& tmp)
+{
+    if (this == &tmp) return *this;
+    if (!tmp.root) clear(root);
+    copy(root, tmp.root);
+    return *this;
+}
+Tree::Tree(const Tree& a)//+
+{
+    copy(root, a.root);
+}
+bool Tree::insert(int x) //+
+{
+    return insert_leaf(root, x);
+}
+Tree::~Tree() //+
+{
+    clear(root);
+}
+void Tree::print() //+
+{
+    print_leaf("", root, false);
+    cout << endl;
+}
+bool Tree::contains(int x) //+
+{
+    return contains_leaf(root, x);
+}
+bool Tree::erase(int x) {
+    if (contains(x) == true) return false;
+    return erase_leaf(root, x);
 }
 
+
+int main() {
+    Tree t;
+    int choise = 0;
+
+    t.insert(20);
+    t.insert(24);
+    t.insert(19);
+    t.insert(21);
+    t.insert(18);
+    t.insert(22);
+    t.print();
+    return 0;
+}
